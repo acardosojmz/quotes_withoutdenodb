00ef44fb75109b9862540082e8cd0f8e7fceae1a
acardosojmz <61549324+acardosojmz@users.noreply.github.com>
2022-11-03 21:50:33 -0600

    hide passwords

--- config/connectors.ts
+++ config/connectors.ts
@@ -4,14 +4,14 @@ import { MongoClient, ClientMariaDB }
 
 
 const mongoClient = new MongoClient();
-const URI_MONGO="mongodb://itvoDeveloper:t0ps3cr3t@localhost:27017/quotes?authMechanism=SCRAM-SHA-256&authSource=admin";
+const URI_MONGO="mongodb://itvoDeveloper:********@localhost:27017/quotes?authMechanism=SCRAM-SHA-256&authSource=admin";
 export const connectorMongoDB = await mongoClient.connect(URI_MONGO);
 
 const CONN_MARIADB = {
     hostname: "localhost",
     port: 3308,
     username: "devDeno",
-    password: "t0ps3cr3t",
+    password: "**********",
     db: "quotes",
     poolSize: 10};
 
